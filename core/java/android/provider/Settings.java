0f3aed983c6bf4f4c2d440e2fb88822c44d60fb2
Vachounet <vachounet@live.fr>
2018-07-21 21:21:37 +0200

    [1/2] base: implement OP like navigation gestures
    Change-Id: I68453353d9d5d6321c1a385744e5b90e2e1a413e
    
    base: add long press home handling to nav gestures
    
    insread of fixed recents use the long press home setting
    
    Change-Id: I94ef0dcb5180c1b305438acf445767fa50f6b670
    
    base: cleanup nav gestures code
    
    Change-Id: I6d412e58e2484cb84307437f67577a5c584c913b
    
    base: cleanup and fix a few nav gestures issues
    
    make sure we dont do anything while on keyguard
    long press should only be active in home location
    make threshold values resource values
    make sure if gestures are disabled no edge events are lost
    
    Import TaskUtils from omni, fixes for DUI
    
    Signed-off-by: phenomenall <venkysaihot@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4393,6 +4393,16 @@ public final class Settings {
                 ANY_INTEGER_VALIDATOR;
 
         /**
+         * @hide
+         */
+        public static final String OMNI_USE_BOTTOM_GESTURE_NAVIGATION =
+                "use_bottom_gesture_navigation";
+
+        /** @hide */
+        private static final Validator OMNI_USE_BOTTOM_GESTURE_NAVIGATION_VALIDATOR =
+                BOOLEAN_VALIDATOR;
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
@@ -4458,6 +4468,7 @@ public final class Settings {
             OMNI_QS_LAYOUT_ROWS,
             OMNI_QS_LAYOUT_ROWS_LANDSCAPE,
             OMNI_QS_PANEL_BG_ALPHA,
+            OMNI_USE_BOTTOM_GESTURE_NAVIGATION,
         };
 
         /**
@@ -4582,6 +4593,7 @@ public final class Settings {
             PRIVATE_SETTINGS.add(OMNI_QS_LAYOUT_ROWS);
             PRIVATE_SETTINGS.add(OMNI_QS_LAYOUT_ROWS_LANDSCAPE);
             PRIVATE_SETTINGS.add(OMNI_QS_PANEL_BG_ALPHA);
+            PRIVATE_SETTINGS.add(OMNI_USE_BOTTOM_GESTURE_NAVIGATION);
         }
 
 
@@ -4679,6 +4691,8 @@ public final class Settings {
             VALIDATORS.put(OMNI_QS_LAYOUT_ROWS, OMNI_QS_LAYOUT_ROWS_VALIDATOR);
             VALIDATORS.put(OMNI_QS_LAYOUT_ROWS_LANDSCAPE, OMNI_QS_LAYOUT_ROWS_LANDSCAPE_VALIDATOR);
             VALIDATORS.put(OMNI_QS_PANEL_BG_ALPHA, OMNI_QS_PANEL_BG_ALPHA_VALIDATOR);
+            VALIDATORS.put(OMNI_USE_BOTTOM_GESTURE_NAVIGATION,
+                    OMNI_USE_BOTTOM_GESTURE_NAVIGATION_VALIDATOR);
         }
 
         /**

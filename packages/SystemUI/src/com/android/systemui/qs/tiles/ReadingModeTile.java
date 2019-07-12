Havoc-OS/android_frameworks_base

SystemUI: Add reading mode tile
Author: Joey <joey@lineageos.org>
Date:   Wed May 16 17:16:06 2018 +0200

    base: add reading mode tile

    Change-Id: I1c515cbeff0dd5a1fa0c00bc9390a66eb104c7f0
    Signed-off-by: Joey <joey@lineageos.org>

Author: Michael Bestas <mkbestas@lineageos.org>
Date:   Sat May 26 00:58:57 2018 +0300

    SystemUI: Fix reading mode tile accessibility strings

    Change-Id: I4f0af56e4947b037e1e2dda79132523d13458818

Author: Paul Keith <javelinanddart@gmail.com>
Date:   Wed Aug 8 18:59:25 2018 +0200

    SystemUI: Update reading mode tile for new API

    Change-Id: I9fbf1b4de30079c987b75e37e52e234fa2f86c5d

Change-Id: I365ee54b2b2adc52d804a5fc1d35690ef5636071
Signed-off-by: Henrique Silva <jhenrique09.mcz@hotmail.com>

packages/SystemUI/src/com/android/systemui/qs/tiles/ReadingModeTile.java
@@ -1,0 +1,111 @@
+/*
+ * Copyright (C) 2018 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.systemui.qs.tiles;
+
+import android.content.Intent;
+import android.service.quicksettings.Tile;
+
+import com.android.systemui.plugins.qs.QSTile.BooleanState;
+import com.android.systemui.qs.QSHost;
+import com.android.systemui.qs.tileimpl.QSTileImpl;
+import com.android.systemui.R;
+
+import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
+
+import com.android.internal.custom.hardware.LineageHardwareManager;
+
+public class ReadingModeTile extends QSTileImpl<BooleanState> {
+
+    private final Icon mIcon = ResourceIcon.get(R.drawable.ic_qs_reader);
+
+    private static final Intent LIVEDISPLAY_SETTINGS =
+            new Intent("com.android.settings.LIVEDISPLAY_SETTINGS");
+
+    private LineageHardwareManager mHardware;
+
+    public ReadingModeTile(QSHost host) {
+        super(host);
+        mHardware = LineageHardwareManager.getInstance(mContext);
+    }
+
+    @Override
+    public BooleanState newTileState() {
+        return new BooleanState();
+    }
+
+    @Override
+    protected void handleClick() {
+        boolean newStatus = !isReadingModeEnabled();
+        mHardware.set(LineageHardwareManager.FEATURE_READING_ENHANCEMENT, newStatus);
+        refreshState();
+    }
+
+    @Override
+    public Intent getLongClickIntent() {
+        return LIVEDISPLAY_SETTINGS;
+    }
+
+    @Override
+    public boolean isAvailable() {
+        return mHardware.isSupported(LineageHardwareManager.FEATURE_READING_ENHANCEMENT);
+    }
+
+    @Override
+    protected void handleUpdateState(BooleanState state, Object arg) {
+        state.value = isReadingModeEnabled();
+        state.icon = mIcon;
+        if (state.value) {
+            state.contentDescription = mContext.getString(
+                    R.string.accessibility_quick_settings_reading_mode_on);
+            state.state = Tile.STATE_ACTIVE;
+        } else {
+            state.contentDescription = mContext.getString(
+                    R.string.accessibility_quick_settings_reading_mode_off);
+            state.state = Tile.STATE_INACTIVE;
+        }
+        state.label = getTileLabel();
+    }
+
+    @Override
+    public CharSequence getTileLabel() {
+        return mContext.getString(R.string.quick_settings_reading_mode);
+    }
+
+    @Override
+    protected String composeChangeAnnouncement() {
+        if (mState.value) {
+            return mContext.getString(
+                    R.string.accessibility_quick_settings_reading_mode_changed_on);
+        } else {
+            return mContext.getString(
+                    R.string.accessibility_quick_settings_reading_mode_changed_off);
+        }
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsEvent.HAVOC_SETTINGS;
+    }
+
+    @Override
+    public void handleSetListening(boolean listening) {
+    }
+
+    private boolean isReadingModeEnabled() {
+        return mHardware.get(LineageHardwareManager.FEATURE_READING_ENHANCEMENT);
+    }
+}

test-vjs/caf_frameworks_base

packages/SystemUI/src/com/android/systemui/qs/tiles/ReadingModeTile.java
@@ -1,0 +1,111 @@
+/*
+ * Copyright (C) 2018 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.systemui.qs.tiles;
+
+import android.content.Intent;
+import android.service.quicksettings.Tile;
+
+import com.android.systemui.plugins.qs.QSTile.BooleanState;
+import com.android.systemui.qs.QSHost;
+import com.android.systemui.qs.tileimpl.QSTileImpl;
+import com.android.systemui.R;
+
+import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
+
+import com.android.internal.custom.hardware.LineageHardwareManager;
+
+public class ReadingModeTile extends QSTileImpl<BooleanState> {
+
+    private final Icon mIcon = ResourceIcon.get(R.drawable.ic_qs_reader);
+
+    private static final Intent LIVEDISPLAY_SETTINGS =
+            new Intent("com.android.settings.LIVEDISPLAY_SETTINGS");
+
+    private LineageHardwareManager mHardware;
+
+    public ReadingModeTile(QSHost host) {
+        super(host);
+        mHardware = LineageHardwareManager.getInstance(mContext);
+    }
+
+    @Override
+    public BooleanState newTileState() {
+        return new BooleanState();
+    }
+
+    @Override
+    protected void handleClick() {
+        boolean newStatus = !isReadingModeEnabled();
+        mHardware.set(LineageHardwareManager.FEATURE_READING_ENHANCEMENT, newStatus);
+        refreshState();
+    }
+
+    @Override
+    public Intent getLongClickIntent() {
+        return LIVEDISPLAY_SETTINGS;
+    }
+
+    @Override
+    public boolean isAvailable() {
+        return mHardware.isSupported(LineageHardwareManager.FEATURE_READING_ENHANCEMENT);
+    }
+
+    @Override
+    protected void handleUpdateState(BooleanState state, Object arg) {
+        state.value = isReadingModeEnabled();
+        state.icon = mIcon;
+        if (state.value) {
+            state.contentDescription = mContext.getString(
+                    R.string.accessibility_quick_settings_reading_mode_on);
+            state.state = Tile.STATE_ACTIVE;
+        } else {
+            state.contentDescription = mContext.getString(
+                    R.string.accessibility_quick_settings_reading_mode_off);
+            state.state = Tile.STATE_INACTIVE;
+        }
+        state.label = getTileLabel();
+    }
+
+    @Override
+    public CharSequence getTileLabel() {
+        return mContext.getString(R.string.quick_settings_reading_mode);
+    }
+
+    @Override
+    protected String composeChangeAnnouncement() {
+        if (mState.value) {
+            return mContext.getString(
+                    R.string.accessibility_quick_settings_reading_mode_changed_on);
+        } else {
+            return mContext.getString(
+                    R.string.accessibility_quick_settings_reading_mode_changed_off);
+        }
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsEvent.HORNS;
+    }
+
+    @Override
+    public void handleSetListening(boolean listening) {
+    }
+
+    private boolean isReadingModeEnabled() {
+        return mHardware.get(LineageHardwareManager.FEATURE_READING_ENHANCEMENT);
+    }
+}
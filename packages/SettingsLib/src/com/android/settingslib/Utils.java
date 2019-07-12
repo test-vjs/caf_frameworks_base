juvevood/android_frameworks_base

Implement weather on lockscreen like Pixels
This use OmniJaws implementations as base, very thanks to Omni team

Icons extracted from Lawnchair, thanks to deletescape and paphonb

WeatherClient is included on vendor/pixelstyle

Our edits:

* Cleanup code
* Adapt to our weather provider
* Slice-ify
* Add required permissions
--------------------------------
* Changed the update interval to 20 min

Change-Id: I05c3d109edb9c38ad11e14dfc17f4dd06a9bcb1d

packages/SettingsLib/src/com/android/settingslib/Utils.java
@@ -30,6 +30,8 @@ import com.android.settingslib.drawable.UserIconDrawable;
 import com.android.settingslib.wrapper.LocationManagerWrapper;
 import java.text.NumberFormat;
 
+import com.android.internal.util.custom.weather.WeatherClient;
+
 public class Utils {
 
     private static final String CURRENT_MODE_KEY = "CURRENT_MODE";
@@ -286,6 +288,7 @@ public class Utils {
                 || pkg.packageName.equals(sServicesSystemSharedLibPackageName)
                 || pkg.packageName.equals(sSharedSystemSharedLibPackageName)
                 || pkg.packageName.equals(PrintManager.PRINT_SPOOLER_PACKAGE_NAME)
+                || pkg.packageName.equals(WeatherClient.SERVICE_PACKAGE)
                 || isDeviceProvisioningPackage(resources, pkg.packageName);
     }
 

packages/SystemUI/src/com/android/systemui/keyguard/KeyguardSliceProvider.java
@@ -25,11 +25,13 @@ import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.database.ContentObserver;
 import android.graphics.drawable.Icon;
 import android.icu.text.DateFormat;
 import android.icu.text.DisplayContext;
 import android.net.Uri;
 import android.os.Handler;
+import android.os.UserHandle;
 import android.provider.Settings;
 import android.service.notification.ZenModeConfig;
 import android.text.TextUtils;
@@ -51,14 +53,17 @@ import androidx.slice.builders.ListBuilder;
 import androidx.slice.builders.ListBuilder.RowBuilder;
 import androidx.slice.builders.SliceAction;
 
+import com.android.internal.util.custom.weather.WeatherClient;
+
 /**
  * Simple Slice provider that shows the current date.
  */
 public class KeyguardSliceProvider extends SliceProvider implements
-        NextAlarmController.NextAlarmChangeCallback, ZenModeController.Callback {
+        NextAlarmController.NextAlarmChangeCallback, ZenModeController.Callback, WeatherClient.WeatherObserver {
 
     public static final String KEYGUARD_SLICE_URI = "content://com.android.systemui.keyguard/main";
     public static final String KEYGUARD_DATE_URI = "content://com.android.systemui.keyguard/date";
+    public static final String KEYGUARD_WEATHER_URI = "content://com.android.systemui.keyguard/weather";
     public static final String KEYGUARD_NEXT_ALARM_URI =
             "content://com.android.systemui.keyguard/alarm";
     public static final String KEYGUARD_DND_URI = "content://com.android.systemui.keyguard/dnd";
@@ -73,6 +78,7 @@ public class KeyguardSliceProvider extends SliceProvider implements
 
     protected final Uri mSliceUri;
     protected final Uri mDateUri;
+    protected final Uri mWeatherUri;
     protected final Uri mAlarmUri;
     protected final Uri mDndUri;
     private final Date mCurrentTime = new Date();
@@ -121,6 +127,7 @@ public class KeyguardSliceProvider extends SliceProvider implements
         mHandler = handler;
         mSliceUri = Uri.parse(KEYGUARD_SLICE_URI);
         mDateUri = Uri.parse(KEYGUARD_DATE_URI);
+        mWeatherUri = Uri.parse(KEYGUARD_WEATHER_URI);
         mAlarmUri = Uri.parse(KEYGUARD_NEXT_ALARM_URI);
         mDndUri = Uri.parse(KEYGUARD_DND_URI);
     }
@@ -129,6 +136,7 @@ public class KeyguardSliceProvider extends SliceProvider implements
     public Slice onBindSlice(Uri sliceUri) {
         ListBuilder builder = new ListBuilder(getContext(), mSliceUri);
         builder.addRow(new RowBuilder(builder, mDateUri).setTitle(mLastText));
+        addWeather(builder);
         addNextAlarm(builder);
         addZenMode(builder);
         addPrimaryAction(builder);
@@ -184,6 +192,62 @@ public class KeyguardSliceProvider extends SliceProvider implements
                 && suppressingNotifications;
     }
 
+    private WeatherClient mWeatherClient;
+    private WeatherClient.WeatherInfo mWeatherInfo;
+    private boolean useMetricUnit;
+
+    protected void addWeather(ListBuilder builder) {
+        if (!WeatherClient.isAvailable(getContext()) || mWeatherInfo == null || mWeatherInfo.getStatus() != WeatherClient.WEATHER_UPDATE_SUCCESS) {
+            return;
+        }
+        int temperatureMetric = mWeatherInfo.getTemperature(true);
+        int temperatureImperial = mWeatherInfo.getTemperature(false);
+        String temperatureText = useMetricUnit ?
+                                 Integer.toString(temperatureMetric) + "°C" :
+                                 Integer.toString(temperatureImperial) + "°F";
+        Icon conditionIcon = Icon.createWithResource(getContext(), mWeatherInfo.getWeatherConditionImage());
+        RowBuilder weatherRowBuilder = new RowBuilder(builder, mWeatherUri)
+                .setTitle(temperatureText)
+                .addEndItem(conditionIcon);
+        builder.addRow(weatherRowBuilder);
+    }
+
+    @Override
+    public void onWeatherUpdated(WeatherClient.WeatherInfo weatherInfo) {
+        if (weatherInfo.getStatus() == WeatherClient.WEATHER_UPDATE_RUNNING){
+            return;
+        }
+        mWeatherInfo = weatherInfo;
+        mContentResolver.notifyChange(mSliceUri, null /* observer */);
+    }
+
+    private WeatherSettingsObserver mWeatherSettingsObserver;
+
+    private class WeatherSettingsObserver extends ContentObserver {
+        WeatherSettingsObserver(Handler handler) {
+            super(handler);
+        }
+
+        void observe() {
+            mContentResolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.WEATHER_LOCKSCREEN_UNIT),
+                    false, this, UserHandle.USER_ALL);
+        }
+
+        @Override
+        public void onChange(boolean selfChange, Uri uri) {
+            super.onChange(selfChange, uri);
+            if (uri.equals(Settings.System.getUriFor(Settings.System.WEATHER_LOCKSCREEN_UNIT))) {
+                updateLockscreenUnit();
+                mContentResolver.notifyChange(mSliceUri, null /* observer */);
+            }
+        }
+
+        public void updateLockscreenUnit() {
+            useMetricUnit = Settings.System.getIntForUser(mContentResolver, Settings.System.WEATHER_LOCKSCREEN_UNIT, 0, UserHandle.USER_CURRENT) == 0;
+        }
+    }
+
     @Override
     public boolean onCreateSliceProvider() {
         mAlarmManager = getContext().getSystemService(AlarmManager.class);
@@ -192,6 +256,13 @@ public class KeyguardSliceProvider extends SliceProvider implements
         mNextAlarmController.addCallback(this);
         mZenModeController = new ZenModeControllerImpl(getContext(), mHandler);
         mZenModeController.addCallback(this);
+        if (WeatherClient.isAvailable(getContext())) {
+            mWeatherSettingsObserver = new WeatherSettingsObserver(mHandler);
+            mWeatherSettingsObserver.observe();
+            mWeatherSettingsObserver.updateLockscreenUnit();
+            mWeatherClient = new WeatherClient(getContext());
+            mWeatherClient.addObserver(this);
+        }
         mDatePattern = getContext().getString(R.string.system_ui_aod_date_pattern);
         registerClockUpdate();
         updateClock();

services/core/java/com/android/server/pm/permission/DefaultPermissionGrantPolicy.java
@@ -80,6 +80,8 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
+import com.android.internal.util.custom.weather.WeatherClient;
+
 /**
  * This class is the policy for granting runtime permissions to
  * platform components and default handlers in the system such
@@ -854,6 +856,12 @@ public final class DefaultPermissionGrantPolicy {
         if (conpro2Package != null) {
             grantRuntimePermissions(conpro2Package, CONTACTS_PERMISSIONS, true, userId);
             grantRuntimePermissions(conpro2Package, STORAGE_PERMISSIONS, true, userId);
+        }
+
+        // Weather client
+        PackageParser.Package weatherClientPackage = getSystemPackage(WeatherClient.SERVICE_PACKAGE);
+        if (weatherClientPackage != null && doesPackageSupportRuntimePermissions(weatherClientPackage)) {
+            grantRuntimePermissions(weatherClientPackage, LOCATION_PERMISSIONS, userId);
         }
 
         if (mPermissionGrantedCallback != null) {

test-vjs/caf_frameworks_base

Signed-off-by: phenomenall <venkysaihot@gmail.com>

packages/SettingsLib/src/com/android/settingslib/Utils.java
@@ -30,6 +30,8 @@ import com.android.settingslib.drawable.UserIconDrawable;
 
 import java.text.NumberFormat;
 
+import com.android.internal.util.stag.weather.WeatherClient;
+
 public class Utils {
 
     private static final String CURRENT_MODE_KEY = "CURRENT_MODE";
@@ -285,6 +287,7 @@ public class Utils {
                 || pkg.packageName.equals(sServicesSystemSharedLibPackageName)
                 || pkg.packageName.equals(sSharedSystemSharedLibPackageName)
                 || pkg.packageName.equals(PrintManager.PRINT_SPOOLER_PACKAGE_NAME)
+                || pkg.packageName.equals(WeatherClient.SERVICE_PACKAGE)
                 || isDeviceProvisioningPackage(resources, pkg.packageName);
     }
 

packages/SystemUI/src/com/android/systemui/keyguard/KeyguardSliceProvider.java
@@ -25,11 +25,13 @@ import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.database.ContentObserver;
 import android.graphics.drawable.Icon;
 import android.icu.text.DateFormat;
 import android.icu.text.DisplayContext;
 import android.net.Uri;
 import android.os.Handler;
+import android.os.UserHandle;
 import android.provider.Settings;
 import android.service.notification.ZenModeConfig;
 import android.text.TextUtils;
@@ -51,14 +53,17 @@ import androidx.slice.builders.ListBuilder;
 import androidx.slice.builders.ListBuilder.RowBuilder;
 import androidx.slice.builders.SliceAction;
 
+import com.android.internal.util.stag.weather.WeatherClient;
+
 /**
  * Simple Slice provider that shows the current date.
  */
 public class KeyguardSliceProvider extends SliceProvider implements
-        NextAlarmController.NextAlarmChangeCallback, ZenModeController.Callback {
+        NextAlarmController.NextAlarmChangeCallback, ZenModeController.Callback, WeatherClient.WeatherObserver {
 
     public static final String KEYGUARD_SLICE_URI = "content://com.android.systemui.keyguard/main";
     public static final String KEYGUARD_DATE_URI = "content://com.android.systemui.keyguard/date";
+    public static final String KEYGUARD_WEATHER_URI = "content://com.android.systemui.keyguard/weather";
     public static final String KEYGUARD_NEXT_ALARM_URI =
             "content://com.android.systemui.keyguard/alarm";
     public static final String KEYGUARD_DND_URI = "content://com.android.systemui.keyguard/dnd";
@@ -73,6 +78,7 @@ public class KeyguardSliceProvider extends SliceProvider implements
 
     protected final Uri mSliceUri;
     protected final Uri mDateUri;
+    protected final Uri mWeatherUri;
     protected final Uri mAlarmUri;
     protected final Uri mDndUri;
     private final Date mCurrentTime = new Date();
@@ -121,6 +127,7 @@ public class KeyguardSliceProvider extends SliceProvider implements
         mHandler = handler;
         mSliceUri = Uri.parse(KEYGUARD_SLICE_URI);
         mDateUri = Uri.parse(KEYGUARD_DATE_URI);
+        mWeatherUri = Uri.parse(KEYGUARD_WEATHER_URI);
         mAlarmUri = Uri.parse(KEYGUARD_NEXT_ALARM_URI);
         mDndUri = Uri.parse(KEYGUARD_DND_URI);
     }
@@ -129,6 +136,7 @@ public class KeyguardSliceProvider extends SliceProvider implements
     public Slice onBindSlice(Uri sliceUri) {
         ListBuilder builder = new ListBuilder(getContext(), mSliceUri);
         builder.addRow(new RowBuilder(builder, mDateUri).setTitle(mLastText));
+        addWeather(builder);
         addNextAlarm(builder);
         addZenMode(builder);
         addPrimaryAction(builder);
@@ -184,6 +192,62 @@ public class KeyguardSliceProvider extends SliceProvider implements
                 && suppressingNotifications;
     }
 
+    private WeatherClient mWeatherClient;
+    private WeatherClient.WeatherInfo mWeatherInfo;
+    private boolean useMetricUnit;
+
+    protected void addWeather(ListBuilder builder) {
+        if (!WeatherClient.isAvailable(getContext()) || mWeatherInfo == null || mWeatherInfo.getStatus() != WeatherClient.WEATHER_UPDATE_SUCCESS) {
+            return;
+        }
+        int temperatureMetric = mWeatherInfo.getTemperature(true);
+        int temperatureImperial = mWeatherInfo.getTemperature(false);
+        String temperatureText = useMetricUnit ?
+                                 Integer.toString(temperatureMetric) + "°C" :
+                                 Integer.toString(temperatureImperial) + "°F";
+        Icon conditionIcon = Icon.createWithResource(getContext(), mWeatherInfo.getWeatherConditionImage());
+        RowBuilder weatherRowBuilder = new RowBuilder(builder, mWeatherUri)
+                .setTitle(temperatureText)
+                .addEndItem(conditionIcon);
+        builder.addRow(weatherRowBuilder);
+    }
+
+    @Override
+    public void onWeatherUpdated(WeatherClient.WeatherInfo weatherInfo) {
+        if (weatherInfo.getStatus() == WeatherClient.WEATHER_UPDATE_RUNNING){
+            return;
+        }
+        mWeatherInfo = weatherInfo;
+        mContentResolver.notifyChange(mSliceUri, null /* observer */);
+    }
+
+    private WeatherSettingsObserver mWeatherSettingsObserver;
+
+    private class WeatherSettingsObserver extends ContentObserver {
+        WeatherSettingsObserver(Handler handler) {
+            super(handler);
+        }
+
+        void observe() {
+            mContentResolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.WEATHER_LOCKSCREEN_UNIT),
+                    false, this, UserHandle.USER_ALL);
+        }
+
+        @Override
+        public void onChange(boolean selfChange, Uri uri) {
+            super.onChange(selfChange, uri);
+            if (uri.equals(Settings.System.getUriFor(Settings.System.WEATHER_LOCKSCREEN_UNIT))) {
+                updateLockscreenUnit();
+                mContentResolver.notifyChange(mSliceUri, null /* observer */);
+            }
+        }
+
+        public void updateLockscreenUnit() {
+            useMetricUnit = Settings.System.getIntForUser(mContentResolver, Settings.System.WEATHER_LOCKSCREEN_UNIT, 0, UserHandle.USER_CURRENT) == 0;
+        }
+    }
+
     @Override
     public boolean onCreateSliceProvider() {
         mAlarmManager = getContext().getSystemService(AlarmManager.class);
@@ -192,6 +256,13 @@ public class KeyguardSliceProvider extends SliceProvider implements
         mNextAlarmController.addCallback(this);
         mZenModeController = new ZenModeControllerImpl(getContext(), mHandler);
         mZenModeController.addCallback(this);
+        if (WeatherClient.isAvailable(getContext())) {
+            mWeatherSettingsObserver = new WeatherSettingsObserver(mHandler);
+            mWeatherSettingsObserver.observe();
+            mWeatherSettingsObserver.updateLockscreenUnit();
+            mWeatherClient = new WeatherClient(getContext());
+            mWeatherClient.addObserver(this);
+        }
         mDatePattern = getContext().getString(R.string.system_ui_aod_date_pattern);
         registerClockUpdate();
         updateClock();

services/core/java/com/android/server/pm/permission/DefaultPermissionGrantPolicy.java
@@ -80,6 +80,8 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
+import com.android.internal.util.stag.weather.WeatherClient;
+
 /**
  * This class is the policy for granting runtime permissions to
  * platform components and default handlers in the system such
@@ -973,6 +975,12 @@ public final class DefaultPermissionGrantPolicy {
         PackageParser.Package pixelwallsPackage = getDefaultProviderAuthorityPackage("com.google.android.apps.wallpaper", userId);
         if (pixelwallsPackage != null) {
             grantRuntimePermissions(pixelwallsPackage, STORAGE_PERMISSIONS, true, userId);
+        }
+
+        // Weather client
+        PackageParser.Package weatherClientPackage = getSystemPackage(WeatherClient.SERVICE_PACKAGE);
+        if (weatherClientPackage != null && doesPackageSupportRuntimePermissions(weatherClientPackage)) {
+            grantRuntimePermissions(weatherClientPackage, LOCATION_PERMISSIONS, userId);
         }
 
         if (mPermissionGrantedCallback != null) {
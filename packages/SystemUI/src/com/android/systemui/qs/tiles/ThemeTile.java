pixeldustproject-p/android_frameworks_base

Base: Add Shishu Nights theme v2.0 [1/3]
 Let's give a darker and transparent alternative to our fancy shishu dreams. Will try to make it the transparent enough to not be an annoyance with the wallpaper.

 Also, the transparency was possible thanks to sai (InFlames03) for making his Oreo subs theme opensource and make easy to noobs like me to know how to do a transparent theme

 Based on how ExtendedUI was made. Based on this commit: https://github.com/AospExtended/platform_frameworks_base/commit/57c1fd82824aa6501b447af8ad30ebbc406d0300

PixelDust Edits:
-Removed statusbar logo
-Moved overlays to vendor
-Rewritten for being used with ThemeAccentUtils

Change-Id: I401fd5f51f3adb4ab393ce6addf0b10ec1d53861
Signed-off-by: spezi77 <spezi7713@gmx.net>

packages/SystemUI/src/com/android/systemui/qs/tiles/ThemeTile.java
@@ -124,6 +124,8 @@ public class ThemeTile extends QSTileImpl<BooleanState> {
                 R.string.system_theme_style_dark, Settings.System.SYSTEM_THEME));
         sStyleItems.add(new ThemeTileItem(3, -1,
                 R.string.system_theme_style_black, Settings.System.SYSTEM_THEME));
+        sStyleItems.add(new ThemeTileItem(4, -1,
+                R.string.system_theme_style_shishunights, Settings.System.SYSTEM_THEME));
     }
 
     private enum Mode {
@@ -296,7 +298,7 @@ public class ThemeTile extends QSTileImpl<BooleanState> {
     }
 
     private ThemeTileItem getThemeItemForStyleMode() {
-        boolean isDark = ThemeAccentUtils.isUsingDarkTheme(mOverlayManager, mCurrentUserId) || ThemeAccentUtils.isUsingBlackTheme(mOverlayManager, mCurrentUserId);
+        boolean isDark = ThemeAccentUtils.isUsingDarkTheme(mOverlayManager, mCurrentUserId) || ThemeAccentUtils.isUsingBlackTheme(mOverlayManager, mCurrentUserId) || ThemeAccentUtils.isUsingShishuNightsTheme(mOverlayManager, mCurrentUserId);
         if (isDark) {
             return new ThemeTileItem(20, R.color.quick_settings_theme_tile_white,
                     R.string.quick_settings_theme_tile_color_white);

packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -2182,6 +2182,10 @@ public class StatusBar extends SystemUI implements DemoMode,
         return ThemeAccentUtils.isUsingBlackTheme(mOverlayManager, mLockscreenUserManager.getCurrentUserId());
     }
 
+    public boolean isUsingShishuNightsTheme() {
+        return ThemeAccentUtils.isUsingShishuNightsTheme(mOverlayManager, mLockscreenUserManager.getCurrentUserId());
+    }
+
     // Unloads the stock dark theme
     public void unloadStockDarkTheme() {
         ThemeAccentUtils.unloadStockDarkTheme(mOverlayManager, mLockscreenUserManager.getCurrentUserId());
@@ -4022,6 +4026,7 @@ public class StatusBar extends SystemUI implements DemoMode,
         // The system wallpaper defines if QS should be light or dark.
         int userThemeSetting = Settings.System.getIntForUser(mContext.getContentResolver(),
                 Settings.System.SYSTEM_THEME, 0, mLockscreenUserManager.getCurrentUserId());
+        boolean useShishuNightsTheme = false;
         boolean useBlackTheme = false;
         boolean useDarkTheme = false;
         if (userThemeSetting == 0) {
@@ -4038,6 +4043,7 @@ public class StatusBar extends SystemUI implements DemoMode,
         } else {
             useDarkTheme = userThemeSetting == 2;
             useBlackTheme = userThemeSetting == 3;
+            useShishuNightsTheme = userThemeSetting == 4;
         }
         if (isUsingDarkTheme() != useDarkTheme) {
             unfuckBlackWhiteAccent(); // Check for black and white accent
@@ -4046,6 +4052,10 @@ public class StatusBar extends SystemUI implements DemoMode,
         if (isUsingBlackTheme() != useBlackTheme) {
             unfuckBlackWhiteAccent(); // Check for black and white accent
             ThemeAccentUtils.setBlackTheme(mOverlayManager, mLockscreenUserManager.getCurrentUserId(), useBlackTheme);
+        }
+        if (isUsingShishuNightsTheme() != useShishuNightsTheme) {
+            unfuckBlackWhiteAccent(); // Check for black and white accent
+            ThemeAccentUtils.setShishuNightsTheme(mOverlayManager, mLockscreenUserManager.getCurrentUserId(), useShishuNightsTheme);
         }
 
         // Lock wallpaper defines the color of the majority of the views, hence we'll use it

test-vjs/caf_frameworks_base

packages/SystemUI/src/com/android/systemui/qs/tiles/ThemeTile.java
@@ -178,6 +178,10 @@ public class ThemeTile extends QSTileImpl<BooleanState> {
                 R.string.system_theme_style_light, Settings.System.SYSTEM_UI_THEME));
         sStyleItems.add(new ThemeTileItem(2, -1,
                 R.string.system_theme_style_dark, Settings.System.SYSTEM_UI_THEME));
+        sStyleItems.add(new ThemeTileItem(3, -1,
+                R.string.system_theme_style_black, Settings.System.SYSTEM_UI_THEME));
+        sStyleItems.add(new ThemeTileItem(4, -1,
+                R.string.system_theme_style_shishunights, Settings.System.SYSTEM_UI_THEME));
     }
 
     private enum Mode {
@@ -350,7 +354,8 @@ public class ThemeTile extends QSTileImpl<BooleanState> {
     }
 
     private ThemeTileItem getThemeItemForStyleMode() {
-        if (ThemeAccentUtils.isUsingDarkTheme(mOverlayManager, mCurrentUserId)) {
+        boolean isDark = ThemeAccentUtils.isUsingDarkTheme(mOverlayManager, mCurrentUserId) || ThemeAccentUtils.isUsingBlackTheme(mOverlayManager, mCurrentUserId) || ThemeAccentUtils.isUsingShishuNightsTheme(mOverlayManager, mCurrentUserId);
+        if (isDark) {
             return new ThemeTileItem(20, R.color.quick_settings_theme_tile_white,
                     R.string.quick_settings_theme_tile_color_white);
         } else {

packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -2315,6 +2315,10 @@ public class StatusBar extends SystemUI implements DemoMode,
         return ThemeAccentUtils.isUsingBlackTheme(mOverlayManager, mLockscreenUserManager.getCurrentUserId());
     }
 
+    public boolean isUsingShishuNightsTheme() {
+        return ThemeAccentUtils.isUsingShishuNightsTheme(mOverlayManager, mLockscreenUserManager.getCurrentUserId());
+    }
+
     // Check for black and white accent overlays
     public void unfuckBlackWhiteAccent() {
         ThemeAccentUtils.unfuckBlackWhiteAccent(mOverlayManager, mLockscreenUserManager.getCurrentUserId());
@@ -4359,6 +4363,7 @@ public class StatusBar extends SystemUI implements DemoMode,
 
         int userThemeSetting = Settings.System.getIntForUser(mContext.getContentResolver(),
                 Settings.System.SYSTEM_UI_THEME, 0, mLockscreenUserManager.getCurrentUserId());
+        boolean useShishuNightsTheme = false;
         boolean useBlackTheme = false;
         boolean useDarkTheme = false;
         if (mCurrentTheme == 0) {
@@ -4375,6 +4380,7 @@ public class StatusBar extends SystemUI implements DemoMode,
         } else {
             useDarkTheme = mCurrentTheme == 2;
             useBlackTheme = mCurrentTheme == 3;
+            useShishuNightsTheme = mCurrentTheme == 4;
         }
         if (isUsingDarkTheme() != useDarkTheme) {
             // Check for black and white accent so we don't end up
@@ -4387,6 +4393,10 @@ public class StatusBar extends SystemUI implements DemoMode,
             // with white on white or black on black
             unfuckBlackWhiteAccent();
             ThemeAccentUtils.setLightBlackTheme(mOverlayManager, mLockscreenUserManager.getCurrentUserId(), useBlackTheme);
+        }
+        if (isUsingShishuNightsTheme() != useShishuNightsTheme) {
+            unfuckBlackWhiteAccent(); // Check for black and white accent
+            ThemeAccentUtils.setShishuNightsTheme(mOverlayManager, mLockscreenUserManager.getCurrentUserId(), useShishuNightsTheme);
         }
 
         // Lock wallpaper defines the color of the majority of the views, hence we'll use it